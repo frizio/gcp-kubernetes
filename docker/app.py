5461c473aacf9a481b266de41c4d4f8af51442f3
Federico Mestrone <federico@moodsdesign.com>
2019-04-09 12:40:13 +0100

    Added local hostname to version page

--- docker/app.py
+++ docker/app.py
@@ -1,6 +1,7 @@
 import locale
 from flask import Flask
 from datetime import datetime
+import socket
 
 
 locale.setlocale(locale.LC_ALL, 'it_IT')
@@ -21,6 +22,7 @@ def hello(name):
 def version():
     s = '<h1>GCP Developer Enablement Program</h1>\n'
     s += '<p>Questa è la demo di Docker, versione 1.0<p>\n'
+    s += '<p>Pagina servita da ' + socket.gethostname() + '<p>\n'
     s += '<p>' + datetime.time(datetime.now()).strftime('%A, %d %B %Y alle %H:%M:%S') + '<p>\n'
     return s
 
