56c47c433bb3381d646de5c6933208a95ca4f080
Federico Mestrone <federico@moodsdesign.com>
2019-04-14 00:04:57 +0100

    Adjustments for file that can't be read

--- k8s/app.py
+++ k8s/app.py
@@ -2,19 +2,19 @@ from flask import Flask
 import pymongo
 import time
 
-dbname = ""
-found = False
-while found:
-    try:
-        with open("/mongo-cfg/database", "r") as f:
-            if f.mode == 'r':
-                dbname = f.read()
-                found = True
-    except:
-        time.sleep(0.5)
+# dbname = ""
+# found = False
+# while found:
+#     try:
+#         with open("/mongo-cfg/database", "r") as f:
+#             if f.mode == 'r':
+#                 dbname = f.read()
+#                 found = True
+#     except:
+#         time.sleep(0.5)
 
 mongo_client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
-mongo_db = mongo_client[dbname]
+mongo_db = mongo_client["mydatabase"]
 mongo_coll = mongo_db["customers"]
 
 app = Flask(__name__)
@@ -24,6 +24,14 @@ app = Flask(__name__)
 def user(name):
     user_object = mongo_coll.find_one({"username": name})
     return user_object
+
+
+@app.route('/shared-data')
+def shared():
+    with open("/mongo-cfg/database", "r") as f:
+        if f.mode == 'r':
+            return f.read()
+    return "Can't read it"
 
 
 if __name__ == '__main__':
