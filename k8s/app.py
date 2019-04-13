2962b5c9bfd7dfc8d06d9e998f563eb131fbbb5b
Federico Mestrone <federico@moodsdesign.com>
2019-04-13 14:00:35 +0100

    Demo with pod creation

--- k8s/app.py
+++ k8s/app.py
@@ -2,19 +2,19 @@ from flask import Flask
 import pymongo
 import time
 
-host = ""
+dbname = ""
 found = False
 while found:
     try:
-        with open("/mongo-cfg/hostname", "r") as f:
+        with open("/mongo-cfg/database", "r") as f:
             if f.mode == 'r':
-                host = f.read()
+                dbname = f.read()
                 found = True
     except:
         time.sleep(0.5)
 
-mongo_client = pymongo.MongoClient("mongodb://%s:27017/" % host)
-mongo_db = mongo_client["mydatabase"]
+mongo_client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+mongo_db = mongo_client[dbname]
 mongo_coll = mongo_db["customers"]
 
 app = Flask(__name__)
